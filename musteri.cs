aee93aea28a3b14a164a7bb6d80513ac3126fc7f
aysesogut03 <aysesogut2003@gmail.com>
2024-12-12 21:58:12 +0300

    Add files via upload

---
+++ musteri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    public class musteri
+    {
+        public static void isim_soyisim_yazdir()
+        {
+            Console.WriteLine("Ayşe Söğüt");
+        }
+    }
+}
